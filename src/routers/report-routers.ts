d57d6409653b65624f3d24eff04a3109e4410114
Higor de Faria <higorpr@gmail.com>
2023-09-05 10:48:04 -0300

    fix:bug fixing at start command

--- src/routers/report-routers.ts
+++ src/routers/report-routers.ts
@@ -2,20 +2,9 @@ import { Router } from "express";
 import { authentication } from "../middleware/auth-middleware";
 import { sendReportEmail, gradeReport } from "../controllers/report-controller";
 import multer from "multer";
-import path from "path";
 
 const reportRouter = Router();
 
-// const storage = multer.diskStorage({
-// 	destination: function (req, file, cb) {
-// 		cb(null, path.join(__dirname, "../uploads/"));
-// 	},
-// 	filename: function (req, file, cb) {
-// 		cb(null, file.originalname);
-// 	},
-// });
-
-// const upload = multer({ storage: storage });
 const upload = multer();
 
 reportRouter
